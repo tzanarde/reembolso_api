ce8cb4ce0059c7b451103a57317ced3c87ff9c6d
tzanarde <zanarde.tiago@gmail.com>
2025-02-16 09:20:02 -0300

    feat: devise parameters permission

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::API
+  before_action :permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+
+  protected
+
+  def permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :email, :password, :name, :role, :manager_user_id, :active ])
+  end
 end
