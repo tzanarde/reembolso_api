faf9d85506f47b0e4f443305b8e37fae1a6398e2
tzanarde <zanarde.tiago@gmail.com>
2025-02-16 13:24:09 -0300

    fix: typo error on tag spec

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -4,6 +4,6 @@ require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
   describe "associations" do
-    it { should validade_presence_of :description }
+    it { should validate_presence_of :description }
   end
 end
