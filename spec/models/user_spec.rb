1439e30f62c1ef1bde4bf14c6bcd2055e4151429
tzanarde <zanarde.tiago@gmail.com>
2025-02-16 15:28:33 -0300

    test: user spec add have_many verification

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,5 +6,6 @@ RSpec.describe User, type: :model do
   describe "associations" do
     it { should have_many(:child_user).class_name("User").with_foreign_key("manager_user_id").dependent(:nullify) }
     it { should belong_to(:manager_user).class_name("User").optional }
+    it { should have_many(:expenses) }
   end
 end
