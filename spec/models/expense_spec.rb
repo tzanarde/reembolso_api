926f8e98457035dff9ba7df128e34a6556ea5dee
tzanarde <zanarde.tiago@gmail.com>
2025-02-17 10:02:11 -0300

    test: expense tag

--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -10,5 +10,26 @@ RSpec.describe Expense, type: :model do
     it { should validate_presence_of :location }
     it { should validate_presence_of :status }
     it { should belong_to(:user) }
+    it { should have_and_belong_to_many(:tags) }
+  end
+
+  describe "relationship" do
+    let!(:user) { create(:user, :employee) }
+    let!(:expense) { create(:expense, :pending, user: user) }
+    let!(:tag) { create(:tag) }
+
+    it 'allows association and access to the tags' do
+      expense.tags << tag
+
+      expect(expense.tags).to include(tag)
+      expect(tag.expenses).to include(expense)
+    end
+
+    it 'allows deletion of a tag' do
+      expense.tags << tag
+      expense.tags.destroy(tag)
+
+      expect(expense.tags).to_not include(tag)
+    end
   end
 end
